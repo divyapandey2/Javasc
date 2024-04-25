de77826f0b65954910c1f386444d987eb8459c76
divyapandey2 <157877661+divyapandey2@users.noreply.github.com>
2024-04-25 10:30:50 +0000

    while

---
+++ 02_Iteration/two.js
@@ -0,0 +1,19 @@
+// let myArray=['ankita','divya','nikhil']
+// let arr=0
+// while (arr<myArray.length){
+//     console.log(`Value is ${myArray[arr]}`);
+//     arr=arr+1
+// }
+
+
+
+// let i=0
+// while (i<10){
+//     console.log(`Value of i is ${i}`);
+//     i=i+2
+// }
+
+
+//do while loop
+
+let
